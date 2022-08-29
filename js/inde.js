1b4c6d48f2d7b254adaf9831d62b5909b0e59058
joneilson lima <73480168+JoneilsonLima@users.noreply.github.com>
2022-08-29 17:01:05 -0300

    Update inde.js

--- js/inde.js
+++ js/inde.js
@@ -5,7 +5,7 @@ sections[0].classList.add('ativo');
 function animaScroll() {
     sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        if(sectionTop < 400) {
+        if(sectionTop < 500) {
             section.classList.add('ativo')
         }
     });
